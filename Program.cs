144a5914ff037b4a38ad0c422ce1ee61d7bb5aa3
dzhumayev <dzhumayev@gmail.com>
2022-08-19 04:38:41 +0600

    Task 4

--- Program.cs
+++ Program.cs
@@ -1,20 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.Write("Введите число a: ");
+﻿Console.Write("Введите число a: ");
 int number_a = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите число b: ");
 int number_b = Convert.ToInt32(Console.ReadLine());
 
+Console.Write("Введите число c: ");
+int number_c = Convert.ToInt32(Console.ReadLine());
 
-if(number_a > number_b) {
-    Console.WriteLine($"Большее a: {number_a}");
-    Console.WriteLine($"Меньшее: b {number_b}");
+int max = number_a;
+if(number_b > max) {
+    max = number_b;
 }
-else if(number_b > number_a) {
-    Console.WriteLine($"Большее b: {number_b}");
-    Console.WriteLine($"Меньшее a: {number_a}");
+if(number_c > max) {
+    max = number_c;
 }
-else {
-    Console.WriteLine("Числа равны");
-}
+
+Console.Write($"Максимальное число: {max}");
