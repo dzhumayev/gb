1a57237d8a3ed0a7daad07abecfbfe1f352eaadd
dzhumayev <dzhumayev@gmail.com>
2022-08-19 04:29:45 +0600

    Task 2

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.Write("Введите число a: ");
+int number_a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+int number_b = Convert.ToInt32(Console.ReadLine());
+
+
+if(number_a > number_b) {
+    Console.WriteLine($"Большее a: {number_a}");
+    Console.WriteLine($"Меньшее: b {number_b}");
+}
+else if(number_b > number_a) {
+    Console.WriteLine($"Большее b: {number_b}");
+    Console.WriteLine($"Меньшее a: {number_a}");
+}
+else {
+    Console.WriteLine("Числа равны");
+}
